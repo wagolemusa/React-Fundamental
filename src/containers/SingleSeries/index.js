b39b3abd51a4101553ab1e36cdddc657482a2893
kisumutalent <homiemusa@gmail.com>
2018-10-04 22:15:49 +0300

    adding routes

---
+++ src/containers/SingleSeries/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import Series from '../Series/index';
+
+class SingleSeries extends Component {
+    render(){
+        console.log(this.props);
+        return(
+
+            <div>
+                <p>Single Series</p>
+            </div>
+        )
+    }
+}
+
+export default SingleSeries;
